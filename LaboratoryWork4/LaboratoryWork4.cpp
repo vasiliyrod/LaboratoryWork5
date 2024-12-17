55d0d1d81589e12e0bbf3a04d93212c883eb4cb3
vasiliyrod <vasya@rodmanich.com>
2024-12-17 19:26:58 +0300

    update

--- LaboratoryWork4/LaboratoryWork4.cpp
+++ LaboratoryWork4/LaboratoryWork4.cpp
@@ -23,38 +23,33 @@ int main()
     LearnedWords emptyWords;
     words.setPair("boy", 2);
     words.setPair("girl", 3);
-    std::cout << "1";
     words.clear();
-    std::cout << "2";
     assert(words == emptyWords);
-    std::cout << "3";
 
 
     //при пересечении любой коллекции с пустой, оператор && возвращает пустую коллекцию (any && empty == empty);
     words.setPair("boy", 2);
     words.setPair("girl", 3);
-    std::cout << "4";
-    //LearnedWords newWords = words && emptyWords;
-    std::cout << "NICE";
     assert((words && emptyWords) == emptyWords);
-    std::cout << "ABOBA";
+
 
     //при пересечении одинаковых коллекций, оператор && возвращает новую коллекцию, равную любой из них (any && any == any);
     LearnedWords words2;
     words2.setPair("boy", 2);
     words2.setPair("girl", 3);
     assert((words && words2) == words);
-    
+    std::cout << "ABOBA";
+
 
     //копия непустой коллекции равна оригиналу, после вставки, удаления или изменения элемента равенство нарушается;
     LearnedWords copyWords(words);
     assert(copyWords == words);
 
-
     //непустая коллекция, сохранённая в файл, равна новой коллекции, загруженной из того же файла.
     words.saveToFile("newfile.txt");
     emptyWords.getFromFile("newfile.txt");
     assert(words == emptyWords);
+
 
     std::cout << "No errors during these tests!";
     return 0;
