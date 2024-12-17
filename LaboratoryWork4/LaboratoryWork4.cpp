280603d1b46504824f08ec5de2c5278135f34c3b
vasiliyrod <vasya@rodmanich.com>
2024-12-17 18:48:25 +0300

    не работает &&

--- LaboratoryWork4/LaboratoryWork4.cpp
+++ LaboratoryWork4/LaboratoryWork4.cpp
@@ -5,32 +5,56 @@
 
 int main()
 {
-    HashMap<std::string, int> words;
-
     //размер коллекции, созданной конструктором по умолчанию, равен нулю;
-   
+    LearnedWords words;
+    assert(words.getLength() == 0);
     
-    //при добавлении нового значения размер коллекции, реализованной на множествах, увеличивается на единицу, а при добавлении имеющегося значения размер остаётся прежним;
 
 
     //при добавлении и удалении нового ключа в коллекцию, реализованную на хеш-таблицах, размер коллекции изменяется на единицу;
+    words.setPair("boy", 1);
+    words.setPair("boy", 3);
+    assert(words.getLength() == 1);
+    words.deletePair("boy");
+    assert(words.getLength() == 0);
 
 
     //непустая коллекция после удаления всех объектов равна коллекции, созданной при помощи конструктора по умолчанию;
+    LearnedWords emptyWords;
+    words.setPair("boy", 2);
+    words.setPair("girl", 3);
+    std::cout << "1";
+    words.clear();
+    std::cout << "2";
+    assert(words == emptyWords);
+    std::cout << "3";
 
 
     //при пересечении любой коллекции с пустой, оператор && возвращает пустую коллекцию (any && empty == empty);
-
+    words.setPair("boy", 2);
+    words.setPair("girl", 3);
+    std::cout << "4";
+    //LearnedWords newWords = words && emptyWords;
+    std::cout << "NICE";
+    assert((words && emptyWords) == emptyWords);
+    std::cout << "ABOBA";
 
     //при пересечении одинаковых коллекций, оператор && возвращает новую коллекцию, равную любой из них (any && any == any);
-
+    LearnedWords words2;
+    words2.setPair("boy", 2);
+    words2.setPair("girl", 3);
+    assert((words && words2) == words);
+    
 
     //копия непустой коллекции равна оригиналу, после вставки, удаления или изменения элемента равенство нарушается;
+    LearnedWords copyWords(words);
+    assert(copyWords == words);
 
 
     //непустая коллекция, сохранённая в файл, равна новой коллекции, загруженной из того же файла.
-
-
+    words.saveToFile("newfile.txt");
+    emptyWords.getFromFile("newfile.txt");
+    assert(words == emptyWords);
 
     std::cout << "No errors during these tests!";
     return 0;
--- LaboratoryWork4/LearnedWords.h
+++ LaboratoryWork4/LearnedWords.h
@@ -4,6 +4,29 @@
 class LearnedWords : public HashMap <std::string, int>
 {
 public:
+	LearnedWords() : HashMap< std::string, int >() {}
+	LearnedWords(const HashMap<std::string, int>& x) : HashMap<std::string, int>(x){}
+
+
 	int getWordsAmountRepeated(int repetetionNum) const; //метод, принимающий число и возвращающий количество ключей, значения (повторения) которых больше или равны этому числу.
 
+
+
 };
+
+int LearnedWords::getWordsAmountRepeated(int repetetionNum) const //метод, принимающий число и возвращающий количество ключей, значения (повторения) которых больше или равны этому числу.
+{
+	int wordsAmount = 0;
+	for (int i = 0; i != BUCKETS_AMOUNT; ++i)
+	{
+		const std::list<std::pair<std::string, int>>& currBucket = buckets[i];
+		for (auto pair : currBucket)
+		{
+			if (pair.second >= repetetionNum)
+			{
+				wordsAmount++;
+			}
+		}
+	}
+	return wordsAmount;
+}
